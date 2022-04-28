f073c9a705d1711b95d1b8c02f38db0f740d969e
Hashim <hashimdev1@gmail.com>
2022-04-29 00:39:58 +0300

    Add error modal component

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 
 import "./ExpenseForm.css";
+import ErrorModal from "../UI/ErrorModal";
 
 const ExpenseForm = (props) => {
 	const [enteredTitle, setEnteredTitle] = useState("");
 	const [enteredAmount, setEnteredAmount] = useState("");
 	const [enteredDate, setEnteredDate] = useState("");
+	const [error, setError] = useState();
 	// const [userInput, setUserInput] = useState({
 	//   enteredTitle: '',
 	//   enteredAmount: '',
@@ -42,6 +44,21 @@ const ExpenseForm = (props) => {
 	const submitHandler = (event) => {
 		event.preventDefault();
 
+		if (enteredTitle.trim().length === 0) {
+			setError({
+				title: "Invalid input",
+				message: "Please enter a valid title (non-empty values).",
+			});
+			return;
+		}
+		if (+enteredAmount < 1) {
+			setError({
+				title: "Invalid amount",
+				message: "Please enter a valid amount (> 0).",
+			});
+			return;
+		}
+
 		const expenseData = {
 			title: enteredTitle,
 			amount: +enteredAmount,
@@ -54,45 +71,58 @@ const ExpenseForm = (props) => {
 		setEnteredDate("");
 	};
 
+	const errorHandler = () => {
+		setError(null);
+	};
+
 	return (
-		<form onSubmit={submitHandler}>
-			<div className="new-expense__controls">
-				<div className="new-expense__control">
-					<label>Title</label>
-					<input
-						type="text"
-						value={enteredTitle}
-						onChange={titleChangeHandler}
-					/>
+		<div>
+			{error && (
+				<ErrorModal
+					title={error.title}
+					message={error.message}
+					onConfirm={errorHandler}
+				/>
+			)}
+			<form onSubmit={submitHandler}>
+				<div className="new-expense__controls">
+					<div className="new-expense__control">
+						<label>Title</label>
+						<input
+							type="text"
+							value={enteredTitle}
+							onChange={titleChangeHandler}
+						/>
+					</div>
+					<div className="new-expense__control">
+						<label>Amount</label>
+						<input
+							type="number"
+							min="0.01"
+							step="0.01"
+							value={enteredAmount}
+							onChange={amountChangeHandler}
+						/>
+					</div>
+					<div className="new-expense__control">
+						<label>Date</label>
+						<input
+							type="date"
+							min="2019-01-01"
+							max="2022-12-31"
+							value={enteredDate}
+							onChange={dateChangeHandler}
+						/>
+					</div>
 				</div>
-				<div className="new-expense__control">
-					<label>Amount</label>
-					<input
-						type="number"
-						min="0.01"
-						step="0.01"
-						value={enteredAmount}
-						onChange={amountChangeHandler}
-					/>
+				<div className="new-expense__actions">
+					<button type="button" onClick={props.onCancel}>
+						Cancel
+					</button>
+					<button type="submit">Add Expense</button>
 				</div>
-				<div className="new-expense__control">
-					<label>Date</label>
-					<input
-						type="date"
-						min="2019-01-01"
-						max="2022-12-31"
-						value={enteredDate}
-						onChange={dateChangeHandler}
-					/>
-				</div>
-			</div>
-			<div className="new-expense__actions">
-				<button type="button" onClick={props.onCancel}>
-					Cancel
-				</button>
-				<button type="submit">Add Expense</button>
-			</div>
-		</form>
+			</form>
+		</div>
 	);
 };
 
